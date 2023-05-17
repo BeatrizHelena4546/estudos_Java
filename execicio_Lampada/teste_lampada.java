31d421f850509a54ddc64fe17a2fa45fa942a09d
Beatriz <75549114+BeatrizHelena4546@users.noreply.github.com>
2023-05-17 15:00:31 -0300

    Add files via upload

--- execicio_Lampada/teste_lampada.java
+++ execicio_Lampada/teste_lampada.java
@@ -4,7 +4,10 @@ public class teste_lampada {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Lampada lampada = new Lampada(true);
+		
+		lampada.mostrarEstado();
+		
 	}
 
 }
