7cb51b5b1430371ac938dddb0bd03f14ad9cbe23
Beatriz <75549114+BeatrizHelena4546@users.noreply.github.com>
2023-05-19 16:32:38 -0300

    Add files via upload

---
+++ exercicio_contaEspecial/Conta_Bancaria.java
@@ -0,0 +1,39 @@
+package exercicio_contaCorrente;
+
+public class Conta_Bancaria {
+	protected String nomeCliente;
+	protected String numConta;
+	
+	protected double saldo;
+
+	public Conta_Bancaria(String nomeCliente, String numConta, double saldo) {
+		this.nomeCliente = nomeCliente;
+		this.numConta = numConta;
+		this.saldo = saldo;
+	}
+	
+	public void sacar(double valor) {
+		if (valor > 0 && valor <= saldo) {
+			saldo-=valor;
+			System.out.println("Saque realizado com sucesso!");
+		}else {
+			System.out.println("Saldo insuficiente para saque :(");
+		}
+	}
+	
+	
+	public void depositar(double valor) {
+		if(valor > 0) {
+			saldo+=valor;
+			System.out.println("Deposito realizado com sucesso!");
+		}else {
+			System.out.println("Valor invalido para deposito :(");
+		}
+	}
+	
+	public void mostrarDados() {
+        System.out.println("Nome do Cliente: " + nomeCliente);;
+        System.out.println("Numero da Conta: " + numConta);
+        System.out.println("Saldo: " + saldo);
+    }
+}
